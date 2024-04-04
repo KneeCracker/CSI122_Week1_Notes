32dfff033ee93e509ddc883235dafc4871d62f03
Jeremy Saetern <jsturnn@gmail.com>
2024-04-03 18:23:54 -0700

    Add project files.

---
+++ CSI122_Week1_Notes/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CSI122_Week1_Notes
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
